4afa11bb8172d0456d621708de398af73ea14630
Arye Shustimov <arye_s@optimove.com>
2019-07-01 19:32:52 +0300

    Updates

---
+++ precache-manifest.9a271adbf5dde0983a4fb57f057b9d8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64154737e97d7df81ae4",
+    "url": "/my-personal-site/static/js/runtime~main.5481a650.js"
+  },
+  {
+    "revision": "7cc0f7d81e6308f73847",
+    "url": "/my-personal-site/static/js/main.b1e07585.chunk.js"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/my-personal-site/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "7cc0f7d81e6308f73847",
+    "url": "/my-personal-site/static/css/main.a1ca28d2.chunk.css"
+  },
+  {
+    "revision": "bd5817f53c591922080b956001d509d3",
+    "url": "/my-personal-site/index.html"
+  }
+];
